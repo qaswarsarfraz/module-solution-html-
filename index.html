0d9b3916ea155664ed4dca257f51031245d45606
Qaswar Sarfraz <158463708+qaswarsarfraz@users.noreply.github.com>
2024-02-07 23:58:29 +0500

    Add files via upload

---
+++ index.html
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+ <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <h1>Our Menu</h1>
+    <div class="section" id="section1">
+        <div id="t1" class="title">Chicken</div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis leo quis nisi malesuada tincidunt. Morbi vitae nisl id eros aliquet tincidunt. Fusce quis lacus ut leo lacinia sagittis. Donec quis arcu sed leo vulputate ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </div>
+    <div class="section" id="section2">
+        <div id="t2"  class="title">Beef</div>
+        <p>Nulla facilisi. Phasellus auctor, sapien quis consequat ultrices, augue eros tincidunt massa, at tincidunt lorem nisl quis eros. Sed id nisl quis nunc sagittis sagittis. Sed quis leo sit amet leo tincidunt sagittis. Sed quis nisl quis nunc sagittis sagittis. Sed quis leo sit amet leo tincidunt sagittis.</p>
+    </div>
+    <div class="section" id="section3">
+        <div id="t3" class="title">Sushi</div>
+        <p>Curabitur quis nisl quis nunc sagittis sagittis. Sed quis leo sit amet leo tincidunt sagittis. Sed quis nisl quis nunc sagittis sagittis. Sed quis leo sit amet leo tincidunt sagittis. Sed quis nisl quis nunc sagittis sagittis. Sed quis leo sit amet leo tincidunt sagittis.</p>
+    </div>
+</body>
+</html>
---
+++ style.css
@@ -0,0 +1,67 @@
+﻿ 
+        <style>
+        * {
+          font-family:cursive;
+            box-sizing: border-box;  
+        }
+        h1 {
+           
+            text-align: center; 
+        }
+        /*  on mobile interface */
+        .section {
+            width: 90%; 
+            background-color:  gray; 
+            border: 1px solid black;
+            position: relative;
+            padding: 10px; 
+            margin: 10px auto;  
+        }
+
+         #t1,#t2,#t3 {
+            position: absolute;
+            top: 0; 
+            right: 0; 
+           
+            border: 2px solid black;
+              font-family:fantasy;
+            padding: 5px; 
+            margin: 5px; 
+          
+        }
+#t1 { 
+   background-color: olivedrab ;            
+        }
+#t2 { 
+  
+            background-color: green; 
+}
+#t3 {
+            background-color: yellow; 
+          
+        }
+
+        .section p {
+            margin-top: 40px; 
+        }
+
+        /* ON tablet  interface */
+        @media (min-width: 768px) and (max-width: 991px) {
+            .section {
+                width: 45%; 
+                float: left; 
+            }
+            #section3 {
+                width: 90%;
+                clear: both; 
+            }
+        }
+
+        /* on desktop interface */
+        @media (min-width: 992px) {
+            .section {
+                width: 30%; 
+                float: left; 
+            }
+        }
+            </style>
